07ea2c900ecd1cbb268a457bc0f7791bf2cb1341
Brian Cairns <brian@prostrategix.com>
2023-06-13 10:15:36 -0400

    close button functionality

--- js/hushmail-forms.js
+++ js/hushmail-forms.js
@@ -1,5 +1,12 @@
+const priorUrl = Document.referrer
+
 const formImage = document.getElementById('formTrigger')
 formImage.addEventListener('mouseenter', (e) => {
     window.open('./form.html', '_top', 'popup')
 })
 
+const close = document.getElementById('close');
+close.addEventListener('click', (e) => {
+    window.close()
+    location.href = priorUrl
+})
