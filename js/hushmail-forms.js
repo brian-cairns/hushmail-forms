7ca5dd557fb8b37dff8cfba5789d6b320d2ddfe0
Brian Cairns <brian@prostrategix.com>
2023-06-13 02:24:23 -0400

    h

--- js/hushmail-forms.js
+++ js/hushmail-forms.js
@@ -1,7 +1,5 @@
-let params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,
-width=600,height=300,left=400,bottom=100, target=_blank`;
 const formImage = document.getElementById('formTrigger')
 formImage.addEventListener('mouseenter', (e) => {
-    window.open('./form.html', 'form', params, 'popup')
+    window.open('./form.html', '_top', 'popup')
 })
 
